7a162342974d26d3a9ec6a3a7e32bc4b74260a11
longBT <bachthanhlong.it@gmail.com>
2025-03-12 18:21:57 +0700

    new code

--- src/component/DetailNumber/LifePeak.jsx
+++ src/component/DetailNumber/LifePeak.jsx
@@ -72,6 +72,201 @@ function LifePeak({ numbers, ages }) {
               strokeWidth={3}
             />
             {/* CAC DIEM VE CAC DINH */}
+
+            {/* CAC DINH */}
+            {/* DINH 4 */}
+            <Circle
+              x={subWidth + 150}
+              y={h4Top / 4}
+              radius={radius + 10}
+              fill="white"
+            />
+            <Label x={subWidth + 150 - 5} y={h4Top / 4 - 3}>
+              <Text
+                align="center"
+                verticalAlign="middle"
+                text="8"
+                fill="red"
+                fontSize={subWidth * 0.09}
+              />
+            </Label>
+
+            {/* DINH 3 */}
+            <Line
+              points={[
+                subWidth + 100,
+                h4Top / 2 - 10,
+                subWidth + 200,
+                h4Top / 2 - 10,
+                subWidth + 150,
+                h4Top / 2 - 10 - 60,
+              ]}
+              closed
+              lineCap="round"
+              lineJoin="round"
+              stroke="black"
+              strokeWidth={3}
+            />
+            <Circle
+              x={subWidth + 150}
+              y={h4Top / 2 - 10 - 60}
+              radius={radius + 5}
+              fill="white"
+            />
+            <Label x={subWidth + 150 - 5} y={h4Top / 2 - 10 - 60 - 8}>
+              <Text
+                align="center"
+                verticalAlign="middle"
+                text="8"
+                fill="red"
+                fontSize={subWidth * 0.09}
+              />
+            </Label>
+            {/*   DINH 3 + TUOI */}
+            <Line
+              points={[
+                subWidth + 130,
+                h4Top / 2 - 10 - 60 - 10,
+                subWidth + 130 - 60,
+                h4Top / 2 - 10 - 60 - 40,
+              ]}
+              lineJoin="round"
+              stroke="blue"
+              strokeWidth={2}
+              dash={[10, 10]}
+            />
+            <Label x={subWidth + 130 - 130} y={h4Top / 2 - 10 - 60 - 60}>
+              <Text
+                align="center"
+                verticalAlign="middle"
+                text="45 "
+                fill="red"
+                fontSize={15}
+              />
+
+              <Text
+                align="center"
+                verticalAlign="middle"
+                x={+20}
+                text="tuổi"
+                fill="#908c89"
+                fontSize={15}
+              />
+
+              <Text
+                align="center"
+                verticalAlign="middle"
+                x={+48}
+                text="(2041)"
+                fill="blue"
+                fontSize={15}
+              />
+            </Label>
+
+            {/* DINH 2 */}
+            <Line
+              points={[
+                subWidth + 150,
+                h4Top / 2 + 50,
+                subWidth + 250,
+                h4Top / 2 + 50,
+                subWidth + 200,
+                h4Top / 2 - 10,
+              ]}
+              closed
+              lineCap="round"
+              lineJoin="round"
+              stroke="black"
+              strokeWidth={3}
+            />
+            <Circle
+              x={subWidth + 200}
+              y={h4Top / 2 - 10}
+              radius={radius + 5}
+              fill="white"
+            />
+            <Label x={subWidth + 200 - 5} y={h4Top / 2 - 15}>
+              <Text
+                align="center"
+                verticalAlign="middle"
+                text="8"
+                fill="red"
+                fontSize={subWidth * 0.09}
+              />
+            </Label>
+            {/* DINH 1 */}
+
+            <Line
+              points={[
+                subWidth + 50,
+                h4Top / 2 + 50,
+                subWidth + 150,
+                h4Top / 2 + 50,
+                subWidth + 100,
+                h4Top / 2 - 10,
+              ]}
+              closed
+              lineCap="round"
+              lineJoin="round"
+              stroke="black"
+              strokeWidth={3}
+            />
+            <Circle
+              x={subWidth + 100}
+              y={h4Top / 2 - 10}
+              radius={radius + 5}
+              fill="white"
+            />
+            <Label x={subWidth + 100 - 5} y={h4Top / 2 - 15}>
+              <Text
+                align="center"
+                verticalAlign="middle"
+                text="8"
+                fill="red"
+                fontSize={subWidth * 0.09}
+              />
+            </Label>
+            {/*   DINH 1 + TUOI */}
+
+            <Line
+              points={[
+                subWidth + 50,
+                h4Top / 2 + 50,
+                subWidth + 50 - 60,
+                h4Top / 2 - 10 - 60 ,
+              ]}
+              lineJoin="round"
+              stroke="blue"
+              strokeWidth={2}
+              dash={[10, 10]}
+            />
+            <Label x={subWidth + 100 - 130} y={h4Top / 2 - 10 - 60}>
+              <Text
+                align="center"
+                verticalAlign="middle"
+                text="45 "
+                fill="red"
+                fontSize={15}
+              />
+
+              <Text
+                align="center"
+                verticalAlign="middle"
+                x={+20}
+                text="tuổi"
+                fill="#908c89"
+                fontSize={15}
+              />
+
+              <Text
+                align="center"
+                verticalAlign="middle"
+                x={+48}
+                text="(2041)"
+                fill="blue"
+                fontSize={15}
+              />
+            </Label>
             {/* DIEM 1 */}
             <Circle
               x={subWidth + 50}
@@ -119,120 +314,6 @@ function LifePeak({ numbers, ages }) {
                 verticalAlign="middle"
                 text="7"
                 fill={gray_color}
-                fontSize={subWidth * 0.09}
-              />
-            </Label>
-
-            {/* CAC DINH */}
-
-            {/* DINH 1 */}
-            <Line
-              points={[
-                subWidth + 50,
-                h4Top / 2 + 50,
-                subWidth + 150,
-                h4Top / 2 + 50,
-                subWidth + 100,
-                h4Top / 2 - 10,
-              ]}
-              closed
-              lineCap="round"
-              lineJoin="round"
-              stroke="black"
-              strokeWidth={3}
-            />
-            <Circle
-              x={subWidth + 100}
-              y={h4Top / 2 - 10}
-              radius={radius + 5}
-              fill="white"
-            />
-            <Label x={subWidth + 100 - 5} y={h4Top / 2 - 10}>
-              <Text
-                align="center"
-                verticalAlign="middle"
-                text="8"
-                fill="red"
-                fontSize={subWidth * 0.09}
-              />
-            </Label>
-
-            {/* DINH 2 */}
-            <Line
-              points={[
-                subWidth + 150,
-                h4Top / 2 + 50,
-                subWidth + 250,
-                h4Top / 2 + 50,
-                subWidth + 200,
-                h4Top / 2 - 10,
-              ]}
-              closed
-              lineCap="round"
-              lineJoin="round"
-              stroke="black"
-              strokeWidth={3}
-            />
-            <Circle
-              x={subWidth + 200}
-              y={h4Top / 2 - 10}
-              radius={radius + 5}
-              fill="white"
-            />
-            <Label x={subWidth + 200 - 5} y={h4Top / 2 - 10}>
-              <Text
-                align="center"
-                verticalAlign="middle"
-                text="8"
-                fill="red"
-                fontSize={subWidth * 0.09}
-              />
-            </Label>
-
-            {/* DINH 3 */}
-            <Line
-              points={[
-                subWidth + 100,
-                h4Top / 2 - 10,
-                subWidth + 200,
-                h4Top / 2 - 10,
-                subWidth + 150,
-                h4Top / 2 - 60,
-              ]}
-              closed
-              lineCap="round"
-              lineJoin="round"
-              stroke="black"
-              strokeWidth={3}
-            />
-            <Circle
-              x={subWidth + 150}
-              y={h4Top / 2 - 60}
-              radius={radius + 5}
-              fill="white"
-            />
-            <Label x={subWidth + 150 - 5} y={h4Top / 2 - 60 - 8}>
-              <Text
-                align="center"
-                verticalAlign="middle"
-                text="8"
-                fill="red"
-                fontSize={subWidth * 0.09}
-              />
-            </Label>
-            {/* DINH 4 */}
-            <Circle
-              x={subWidth + 150}
-              y={h4Top / 4}
-              radius={radius + 10}
-              fill="white"
-            />
-            <Label x={subWidth + 150 - 5} y={h4Top / 4 - 3}>
-              <Text
-                align="center"
-                verticalAlign="middle"
-                text="8"
-                fill="red"
                 fontSize={subWidth * 0.09}
               />
             </Label>
