1dc47901ba20318a1ac355b823eaf3068e9b0ba3
longBT <bachthanhlong.it@gmail.com>
2025-05-17 12:05:40 +1200

    upd form

--- src/component/DetailNumber/ChartDateName.jsx
+++ src/component/DetailNumber/ChartDateName.jsx
@@ -1,7 +1,7 @@
 import { Tag } from "antd";
 import React, { useEffect, useRef, useState } from "react";
 import { Stage, Layer, Rect, Text, Label } from "react-konva";
-
+import DrawCellDateName from "./SubComponent/DrawCellDateName";
 const ChartDateName = ({
   numbersData,
   color = "red",
@@ -24,70 +24,22 @@ const ChartDateName = ({
   const canvasEl = useRef(null);
 
   useEffect(() => {
-    const width = canvasEl?.current?.offsetWidth;
-    setWLeftPanel(width);
+    const handleResize = () => {
+      const width = canvasEl?.current?.offsetWidth;
+      const widthWindow = window.innerWidth;
+      widthWindow < 768 ? setWLeftPanel(width * 1.3) : setWLeftPanel(width);
+    };
+
+    handleResize(); // Gọi lần đầu khi mount
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
   }, []);
 
   const wMatrix = wRightPanel * 0.45;
   const hMatrix = (wMatrix / 3) * 3;
-
-  const DrawCell = () => {
-    const rects = [];
-
-    for (let x = 0; x < 3; x++) {
-      const xx = (wMatrix / 3) * x + 3;
-      for (let y = 0; y < 3; y++) {
-        const yy = hMatrix - (hMatrix / 3) * (y + 1) + 5;
-
-        const stt = y + 1 + 3 * x;
-        let text =
-          stt === 11
-            ? 20
-            : stt === 12
-            ? 30
-            : stt === 13
-            ? 22
-            : stt === 14
-            ? 11
-            : stt === 15
-            ? 33
-            : stt;
-        rects.push(
-          <React.Fragment key={`${x}-${y}`}>
-            {/* Vẽ viền ô */}
-            <Rect
-              x={xx}
-              y={yy}
-              width={wMatrix / 3 - 10}
-              height={hMatrix / 3 - 10}
-              fill="white"
-              stroke="black" // Thêm viền đen
-              strokeWidth={2} // Độ dày viền
-            />
-            {/* Hiển thị số bên trong */}
-            <Text
-              x={xx}
-              y={yy}
-              width={wMatrix / 3 - 10}
-              height={hMatrix / 3 - 10}
-              fontStyle="bold"
-              fill={color}
-              align="center"
-              text={
-                amountNumber.hasOwnProperty(text)
-                  ? text + "^" + amountNumber[text]
-                  : ""
-              }
-              verticalAlign="middle"
-              fontSize={wMatrix * 0.06}
-            />
-          </React.Fragment>
-        );
-      }
-    }
-
-    return rects;
-  };
 
   const scrollToSection = (id) => {
     const element = document.getElementById(id);
@@ -97,13 +49,16 @@ const ChartDateName = ({
   };
 
   return (
-    <div class="col-6 my-3" ref={canvasEl}>
+    <div class="col-sm-12 col-md-6 my-3" ref={canvasEl}>
       <div className=" d-flex justify-content-center ">
         {/* Vẽ biểu đồ bằng React Konva */}
         {wMatrix && (
-          <Stage width={wMatrix} height={hMatrix}>
-            <Layer>{DrawCell()}</Layer>
-          </Stage>
+          <DrawCellDateName
+            wMatrix={wMatrix}
+            hMatrix={hMatrix}
+            amountNumber={amountNumber}
+            color={color}
+          />
         )}
       </div>
       {/* Nút bấm */}
